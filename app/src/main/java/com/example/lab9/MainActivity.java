06fc8d8bb413f83ca939b1a5613a5a24d0710f2a
ellieburger <lee.liz2002ilovenh1d520@gmail.com>
2023-12-01 14:35:34 +0800

    7.5 very happy

---
+++ app/src/main/java/com/example/lab9/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.lab9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_clock;
+    private Button start;
+    private Boolean flag = false;
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Bundle b = intent.getExtras();
+            tv_clock.setText(String.format(Locale.getDefault(),"%02d:%02d:%02d", b.getInt("H"), b.getInt("H"), b.getInt("S")));
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_clock = findViewById(R.id.tv_clock);
+        start = findViewById(R.id.start);
+
+        registerReceiver(receiver, new IntentFilter("MyMessage"));
+
+        flag = MyService.flag;
+
+        if(flag)
+            start.setText("暫停");
+        else
+            start.setText("開始");
+        start.setOnClickListener(v -> {
+            flag = !flag;
+            if(flag){
+                start.setText("暫停");
+                Toast.makeText(this, "計時開始", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                start.setText("開始");
+                Toast.makeText(this, "計時暫停", Toast.LENGTH_SHORT).show();
+            }
+            startService(new Intent(this, MyService.class).putExtra("flag", flag));
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+}
